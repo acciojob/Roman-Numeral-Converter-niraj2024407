26cff97232a561e801b12cef4070ae5ed86065bd
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-14 15:21:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,14 +10,38 @@ function convertToRoman(num) {
     };
 
   //your code here
+	// Extended mapping with subtractive notation
+    const romanMap = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
+	let result = '';
+	for(let [symbol, value] of romanMap){
+		while(num>=value){
+			result += symbol;
+			num -= value;
+		}
+	}
+	return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
 
-
-
-
+console.log(convertToRoman(14));   // XIV
+console.log(convertToRoman(798));  // DCCXCVIII
+console.log(convertToRoman(100000)); // M repeated 100 times
 // do not edit below this line
 module.exports = convertToRoman
